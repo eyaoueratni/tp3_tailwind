ee0d5b4a897888653976199c4e37b7f361e4e1a1
eya ouertani <ouertaniaya50@gmail.com>
2024-05-19 21:07:27 +0100

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+import { CreateComponent } from './post/create/create.component';
+import { EditComponent } from './post/edit/edit.component';
+import { IndexComponent } from './post/index/index.component';
+import { ViewComponent } from './post/view/view.component';
+export const routes: Routes = [
+{ path: 'post', redirectTo: 'post/index',
+pathMatch: 'full'},
+{ path: 'post/index', component: IndexComponent },
+{ path: 'post/:postId/view', component:
+ViewComponent },
+{ path: 'post/create', component: CreateComponent
+},
+{ path: 'post/:postId/edit', component:
+EditComponent }
+];
